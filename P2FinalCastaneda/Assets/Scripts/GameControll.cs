3bc9f5a8c551e477854e7d60779f60f7a23d3a2c
ethancastaneda1 <726750@student.dusd.net>
2024-12-17 10:10:42 -0800

    Final Submit
    YAHO Final SUBBmitted

--- P2FinalCastaneda/Assets/Scripts/GameControll.cs
+++ P2FinalCastaneda/Assets/Scripts/GameControll.cs
@@ -5,12 +5,13 @@ using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameControll : MonoBehaviour
 {
     public static GameControll instance;
     public GameObject gameOverText;
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
     private int score = 0;
